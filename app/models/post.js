ff5600e122654aac8a105c5e635f2e2ff78fdd81
Aditi Anomita Mohanty <aditi.anomita@gmail.com>
2015-11-14 12:52:55 +0530

    yolo

--- app/models/post.js
+++
@@ -1,9 +0,0 @@
-import DS from "ember-data";
-
-
-export default DS.Model.extend({
-	title: DS.attr('string'),
-	author: DS.attr('string'),
-	date: DS.attr('string'),
-	content: DS.attr('string')
-});
