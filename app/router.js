9af18b3d44b5ca95d59a3835cd5350010dd91c7d
Aditi Anomita Mohanty <aditi.anomita@gmail.com>
2015-12-03 15:50:47 +0530

    Fresh

--- app/router.js
+++ app/router.js
@@ -9,22 +9,14 @@ Router.map(function() {
   this.route('newsfeed', { path: '/' } );
   this.route('createPost' , { path: '/create' });
 
-
-  this.route('m-posts', { path: '/mposts'}, function() {
-    this.route('index', { path: '/mposts/index' });
-    this.route('create', { path: '/mposts/create' });
-  });
-
   this.route('index', { path: '/index'} );
 
   this.route('posts', { path: '/feed' }, function(){
   	this.route('post', { path : '/:post_id'});
   });
-
-
+  this.route('temp-typeahead');
 
   this.route('404', { path: '/*wildcard' });
-  this.route('temp-typeahead');
 });
 
 export default Router;
